a9d42e888bb86540e32d158f76cc3447901648b5
yangkun <45101218@qq.com>
2017-12-11 22:14:27 +0800

    pipe connection

---
+++ Pipe/pipe.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: pipe.c
+	> Author:yangkun 
+	> Mail:yangkungetit@163.com 
+	> Created Time: Mon 11 Dec 2017 10:13:14 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main()
+{
+    return 0;
+}
